08da64a4ac05d5429b2d28e9005461e9512c331f
justinji <justin5562587@gmail.com>
2019-07-01 21:03:03 +0800

    edit

--- src/main/java/com/justin/system/SystemApplication.java
+++ src/main/java/com/justin/system/SystemApplication.java
@@ -2,8 +2,9 @@ package com.justin.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SystemApplication {
 
     public static void main(String[] args) {
